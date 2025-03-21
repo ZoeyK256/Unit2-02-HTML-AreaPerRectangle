6f38be84891e7006af2429794595f4da2fafe2f0
Zoey Kariyal <zoey.kariyal@stu.ocsb.ca>
2025-03-21 15:01:13 +0000

    added buttons with JS functions

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function calculateArea() {
+    // this function does basic math
+    document.getElementById('multiply-math').innerHTML = 'The area is = ' + (7 * 11) + 'm²'
+
+}
+
+function calculatePerimeter() {
+    // this function does basic math
+    document.getElementById('add-math').innerHTML = 'The perimeter is = ' + (7 + 7 + 11 + 11) + 'm'
+
+}
